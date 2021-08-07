889f1996344ce304773ee9f416dc29b2e3e23861
Ivan <ivnknv@zohomail.com>
2021-08-08 01:23:26 +0300

    Add outlets and actions for buttons and text field

--- AwasomeBrowser/ViewController.swift
+++ AwasomeBrowser/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://www.apple.com/ru/")
+        let request = URLRequest(url: url!)
+        
+        webView.load(request)
+        webView.allowsBackForwardNavigationGestures = true
     }
 
-
+    @IBAction func backButtonAction(_ sender: UIButton) {
+    }
+    @IBAction func forwardButtonAction(_ sender: UIButton) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 }
 
